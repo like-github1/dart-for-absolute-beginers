255cc205e1989a9fa2741f75ddc2d0f73900f367
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-13 11:26:39 -0800

    .

---
+++ bin/dart_for_absolute_beginers.dart
@@ -0,0 +1,8 @@
+void main() {
+  print("hello dart programers");
+  print(4); //to print numbers
+  print(4 +
+      43); //you can also print the sum and sub, multi, and substruction of a number
+  print(50 ~/ 4); //print only digits
+  print(50 % 8); //print the remainder of the number after dividing
+}
