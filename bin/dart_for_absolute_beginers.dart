daad6c933d3c6c83e113ec80333d0d01742f6448
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-13 11:12:03 -0800

    Update dart_for_absolute_beginers.dart
    print string in dart

--- bin/dart_for_absolute_beginers.dart
+++ bin/dart_for_absolute_beginers.dart
@@ -1,4 +1,4 @@
 void main(List<String> arguments) {
- //in these Tutorials we learn everything abut Dart Programing Languges
-  //we learn all fundamental of Dart programing languges
+print("hello dart programers");
+
 }
