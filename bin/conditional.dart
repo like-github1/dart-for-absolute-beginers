ff6b2df18eaebd16da4991a9614bc75923f7cb31
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-14 02:13:51 -0800

    Switch-case-Statement

--- bin/conditional.dart
+++ bin/conditional.dart
@@ -4,9 +4,28 @@ void main() {
   //02 (CONDITIONS)??"GUEST";
   int a = 4;
   int b = 6;
+// ignore: prefer_typing_uninitialized_variables
   var name;
   int smallnumber = (a < b) ? a : b;
   String nametoprint = name ?? "tom";
   print(nametoprint);
   print("$smallnumber is smaller");
+  int age = 0;
+
+  //Switch case Statement
+  //for switch case statement the parameter must be intiger and String
+  switch (age) {
+    case 20:
+      print("your age is 20");
+      break;
+    case 30:
+      print("your age is 30");
+      break;
+    case 40:
+      print("your age is 40");
+      break;
+    default:
+      print("this is the Default age");
+      break;
+  }
 }
